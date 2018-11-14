f85bdb493ba26fb2175389342f042c04055c9625
Isuru Fernando <isuruf@gmail.com>
2018-11-14 01:59:54 -0600

    Move updating CXXFLAGS

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
+CXXFLAGS=$(echo "${CXXFLAGS}" | sed "s/-std=c++17/-std=c++14/g")
+
 ./autogen.sh
 ./configure --disable-debug --disable-dependency-tracking --prefix=${PREFIX} --enable-cplusplus
 
-CXXFLAGS=$(echo "${CXXFLAGS}" | sed "s/-std=c++17/-std=c++14/g")
 make
 make check
 make install
