c72666c2460b3529ad1642f2ba43253a8297137d
Isuru Fernando <isuruf@gmail.com>
2018-11-14 01:21:37 -0600

    Use C++14 for now

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,6 +2,8 @@
 
 ./autogen.sh
 ./configure --disable-debug --disable-dependency-tracking --prefix=${PREFIX} --enable-cplusplus
+
+CXXFLAGS=$(echo "${CXXFLAGS}" | sed "s/-std=c++17/-std=c++14/g")
 make
 make check
 make install
