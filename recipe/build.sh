1a4dc94ad09ff0cf08ba2799b55d68f68c35b612
Isuru Fernando <isuruf@gmail.com>
2022-02-14 22:27:59 -0600

    link to librt

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,11 +2,15 @@
 
 CXXFLAGS=$(echo "${CXXFLAGS}" | sed "s/-std=c++17/-std=c++14/g")
 
+if [[ "$target_platform" == "linux-64" ]]; then
+  export LIBS='-lrt'
+fi
+
 ./autogen.sh
 ./configure --disable-debug --disable-dependency-tracking --prefix=${PREFIX} --enable-cplusplus
 
 make
-if [[ "${CONDA_BUILD_CROSS_COMPILATION}" != "1" ]]; then
-make check
+if [[ "${CONDA_BUILD_CROSS_COMPILATION}" != "1" || "${CROSSCOMPILING_EMULATOR}" != "" ]]; then
+  make check
 fi
 make install
