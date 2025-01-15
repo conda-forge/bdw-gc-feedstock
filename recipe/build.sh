770218942916843b9bbb11e7345c677273d1fd72
Daniel Nachun <daniel.nachun@gmail.com>
2025-01-14 22:31:15 -0800

    skip check on linux-aarch64

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,7 +6,9 @@ CXXFLAGS=$(echo "${CXXFLAGS}" | sed "s/-std=c++17/-std=c++14/g")
 ./configure --disable-debug --disable-dependency-tracking --prefix=${PREFIX} --enable-cplusplus
 
 make
-if [[ "${CONDA_BUILD_CROSS_COMPILATION}" != "1" ]]; then
+if [[ "${CONDA_BUILD_CROSS_COMPILATION}" != "1" && ${target_platform} != "linux-aarch64" ]]; then
+# Skip check on `linux-aarch64` due to segfaults:
+# https://src.fedoraproject.org/rpms/gc/blob/rawhide/f/gc.spec#_81
 make check
 fi
 make install
