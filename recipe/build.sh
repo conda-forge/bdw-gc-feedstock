ecd4881fb887754df43b8b04f5acb9fd2b6268d7
Isuru Fernando <isuruf@gmail.com>
2025-01-22 14:24:31 +0530

    fix testing

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,7 +7,7 @@ CXXFLAGS=$(echo "${CXXFLAGS}" | sed "s/-std=c++17/-std=c++14/g")
 
 make -j${CPU_COUNT}
 if [[ "${CONDA_BUILD_CROSS_COMPILATION}" != "1" || "${CROSSCOMPILING_EMULATOR}" != "" ]]; then
-  if [[ ${target_platform} != "linux-aarch64" ]]; then
+  if [[ ${target_platform} == "linux-aarch64" ]]; then
     echo "skip check on `linux-aarch64` due to segfaults"
     # https://src.fedoraproject.org/rpms/gc/blob/rawhide/f/gc.spec#_81
   else
